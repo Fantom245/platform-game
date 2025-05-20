0f00481930a673e7c92982607bfe63f034ed44f6
unknown <gyrkalomas@gmail.com>
2025-05-21 02:16:45 +0300

    add Dash

--- entities/player.py
+++ entities/player.py
@@ -27,6 +27,13 @@ class Player:
         self.attack_frame = 0
         self.attack_timer = 0
 
+        self.is_dashing = False
+        self.dash_time = 0
+        self.dash_duration = 200  # мс
+        self.dash_speed = 20
+        self.dash_cooldown = 1000  # мс
+        self.last_dash = -self.dash_cooldown
+
     def handle_attack(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_j] and not self.is_attacking:
@@ -34,12 +41,22 @@ class Player:
             self.attack_frame = 0
             self.attack_timer = pygame.time.get_ticks()
 
+    def handle_dash(self):
+        keys = pygame.key.get_pressed()
+        now = pygame.time.get_ticks()
+        if keys[pygame.K_LSHIFT] and not self.is_dashing and (now - self.last_dash) >= self.dash_cooldown:
+            self.is_dashing = True
+            self.dash_time = now
+            self.last_dash = now
+
     def update(self, platforms):
         self.handle_attack()
+        self.handle_dash()
+
+        now = pygame.time.get_ticks()
 
         # Обработка атаки
         if self.is_attacking:
-            now = pygame.time.get_ticks()
             if now - self.attack_timer > 70:
                 self.attack_frame += 1
                 self.attack_timer = now
@@ -50,22 +67,34 @@ class Player:
         keys = pygame.key.get_pressed()
         dx = 0
 
-        if keys[pygame.K_a]:
-            dx = -PLAYER_SPEED
-        elif keys[pygame.K_d]:
-            dx = PLAYER_SPEED
+        # Обработка Dash
+        if self.is_dashing:
+            if now - self.dash_time < self.dash_duration:
+                if keys[pygame.K_a]:
+                    dx = -self.dash_speed
+                elif keys[pygame.K_d]:
+                    dx = self.dash_speed
+            else:
+                self.is_dashing = False
+        else:
+            # Обычное движение
+            if keys[pygame.K_a]:
+                dx = -PLAYER_SPEED
+            elif keys[pygame.K_d]:
+                dx = PLAYER_SPEED
 
-        if keys[pygame.K_w] and self.on_ground:
-            self.vel_y = JUMP_FORCE
-            self.on_ground = False
+            # Прыжок
+            if keys[pygame.K_w] and self.on_ground:
+                self.vel_y = JUMP_FORCE
+                self.on_ground = False
 
-        # Применение гравитации всегда (но vel_y будет = 0 при приземлении)
+        # Применение гравитации
         self.vel_y += GRAVITY
         dy = self.vel_y
 
         self.on_ground = False  # Сброс перед проверкой
 
-        # Столкновение с платформами (в том числе пол и "земля")
+        # Столкновение с платформами
         for platform in platforms:
             future_rect = self.rect.move(0, dy)
             if future_rect.colliderect(platform.rect):
@@ -82,6 +111,7 @@ class Player:
             self.vel_y = 0
             self.on_ground = True
 
+        # Применение перемещения
         self.rect.x += dx
         self.rect.y += dy
 
